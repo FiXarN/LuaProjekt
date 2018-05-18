00f57031511a7fd2de4d32f88fd817e2eff6d52f
FREDRIK CARLSSON <frcc15@student.bth.se>
2018-05-18 15:41:13 +0200

    addBox done

--- Projekt/Lua_Irrlicht_BTH_template/Main.cpp
+++ Projekt/Lua_Irrlicht_BTH_template/Main.cpp
@@ -21,7 +21,7 @@ static int cameraFunction(lua_State* L);
 static int snapshot(lua_State* L);
 
 irr::scene::IAnimatedMeshSceneNode* node;
-irr::scene::ISceneNode * meshNode;
+irr::scene::ISceneNode * boxNode;
 irr::scene::ISceneManager* smgr;
 
 void ConsoleThread(lua_State* L) {
@@ -75,15 +75,6 @@ int main()
 
 	device->getCursorControl()->setVisible(false);
 
-	/*---------------AddBox----------------------*/
-	meshNode = smgr->addCubeSceneNode();
-	if (meshNode) {
-		//meshNode->setPosition(pos);
-		meshNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-	}
-
-	/*-------------------------------------------*/
-
 	/*--------------------------------------------------------------------*/
 	lua_register(L, "addMesh", addMesh);
 	lua_register(L, "addBox", addBox);
@@ -122,29 +113,46 @@ static int addMesh(lua_State* L) {
 	return 0;
 }
 
+float counter = 0;
 static int addBox(lua_State* L) {
 
 	irr::core::vector3df pos;
-	irr::f32 size;
+	irr::f32 size = 0.0f;
 	std::string name;
 
 	luaL_argcheck(L, lua_istable(L, 1), -1, "Error position table");
+	luaL_argcheck(L, lua_isnumber(L, 2), -1, "Error size, must be a number");
 
-	pos.X = lua_tonumber(L, 1);
-	pos.Y = lua_tonumber(L, 2);
-	pos.Z = lua_tonumber(L, 3);
-	//size = lua_tonumber(L, 4);
+	if (lua_gettop(L) == 3) {
+		if (lua_isnumber(L, 2)) {
+			size = lua_tonumber(L, 2);
+		}
+		if (lua_isstring(L, 3)) {
+			name = lua_tostring(L, 3);
+		}
+	}
+	else {
+		counter++;
+		name = std::to_string(counter);
+	}
 
+	lua_rawgeti(L, 1, 1); //Hämtar x värdet och lägger högst upp på stacken
+	lua_rawgeti(L, 1, 2); //Hämtar y värdet och lägger högst upp på stacken
+	lua_rawgeti(L, 1, 3); //Hämtar z värdet och lägger högst upp på stacken
+
+	if (lua_isnumber(L, -1) || lua_isnumber(L, -2) || lua_isnumber(L, -3)) {
+		pos.X = lua_tonumber(L, -3);
+		pos.Y = lua_tonumber(L, -2);
+		pos.Z = lua_tonumber(L, -1);
+	}
 
 	///*---------------AddBox----------------------*/
-	//meshNode = smgr->addCubeSceneNode();
-	//if (meshNode) {
-	//	meshNode->setPosition(pos);
-	//	meshNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-	//}
+	boxNode = smgr->addCubeSceneNode(size, 0, -1, pos, irr::core::vector3df(0, 0, 0), irr::core::vector3df(1, 1, 1));
+	if (boxNode) {
+		boxNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+	}
 
 	///*-------------------------------------------*/
-
 	return 0;
 }
 
